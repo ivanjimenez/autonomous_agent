f2cc6992f80d9af9296fe81d641a64965295693b
ivanjimenezfp@gmail.com <ivanjimenezfp@gmail.com>
2024-06-10 17:04:54 -0400

    change handle and behaviour works

--- agent/agent.py
+++ agent/agent.py
@@ -5,12 +5,20 @@ import secrets
 from datetime import datetime
 from agent.abstract_agent import AbstractAgent
 from helpers.color_text_line import color_line
+import logging
 
 # For modules
 from behaviours.simple_message_generator import SimpleMessageGenerator
 from handlers.filter_handler import FilterHandler
 class Agent(AbstractAgent):
-    def __init__(self, name):
+
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    def __init__(self, name) -> None:
         # Init arguments
         self.name = name
         self.outboxqueue = asyncio.Queue()
@@ -18,37 +26,51 @@ class Agent(AbstractAgent):
         self.other_agent = None
         self.handle = None
         self.behaviour = None
+        self.iterations = 0
 
     def set_other_agent(self, other_agent):
         # Set Agent
         self.other_agent = other_agent
 
     def register_handle(self, handle: FilterHandler):
-        # Registiring handle
+        # Registering handle
         self.handle = handle
 
     def register_behaviour(self, behaviour: SimpleMessageGenerator):
-        # Registiring behaviour
+        # Registering behaviour
         self.behaviour = behaviour
 
     async def process_messages(self):
         """Process incoming messages."""
-        message = await self.inboxqueue.get()
-        filtered_message = self.handle.find_word(message)
-        current_time = datetime.now().strftime('%H:%M:%S.%f')[:-4]
-        print(f"{current_time} {self.name} received: {filtered_message}")
+        while True: # self.iterations < 700:
+            try:
+                message = await self.inboxqueue.get()
+                filtered_message = self.handle.find_word(message)
+                # current_time = datetime.now().strftime('%H:%M:%S.%f')[:-4]
+                # print(f"{current_time} {self.name} received: {filtered_message}")
+                logging.info("%s sending: %s", self.name, filtered_message)
+                self.iterations += 1
+            except asyncio.QueueEmpty:
+                await asyncio.sleep(0.1) 
 
     async def generate_messages(self):
         """Generate and send messages to the other agent."""
-        message = f"{self.behaviour.process_message()} id {secrets.token_hex(5)}"
-        message = color_line(message)
-        current_time = datetime.now().strftime('%H:%M:%S.%f')[:-4]
-        print(f"{current_time} {self.name} sending: {message}")
-        await self.other_agent.inboxqueue.put(message)
-        await asyncio.sleep(2)
+        while True: #self.iterations < 700:
+            message = f"{self.behaviour.process_message()} id {secrets.token_hex(5)}"
+            message = color_line(message)
+            # current_time = datetime.now().strftime('%H:%M:%S.%f')[:-4]
+            # print(f"{current_time} {self.name} sending: {message}")
+            logging.info("%s sending: %s", self.name, message)
+            self.iterations += 100
+            try:
+                await self.other_agent.inboxqueue.put(message)
+                await asyncio.sleep(2)
+            except asyncio.QueueFull:
+                logging.exception('%s could not send: queue is full', self.name)
+                
 
     async def run(self):
         """Main loop to generate and process messages."""
-        while True:
-            await self.generate_messages()
-            await self.process_messages()
+        task1 = asyncio.create_task(self.generate_messages())
+        task2 = asyncio.create_task(self.process_messages())
+        await asyncio.gather(task1, task2)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Asynchronous
 import asyncio
 import gc
+import time
+from typing import Callable, Tuple
 
 # For agent
 from agent.agent import Agent
@@ -8,26 +10,15 @@ from behaviours.simple_message_generator import SimpleMessageGenerator
 from handlers.filter_handler import FilterHandler
 
 
-async def main():
+async def main(agent_factory: Callable[[], Tuple[Agent, Agent]]):
     """
         Main method
     """
     tasks = []
 
-    # Setting behaviour
-    behaviour_option = [
-        'hello', 'sun', 'world', 'space', 'moon',
-        'crypto', 'sky', 'ocean', 'universe'
-    ]
-    message_generator = SimpleMessageGenerator(alphabet=behaviour_option)
-
-    # Setting handle
-    handle_option = 'hello'
-    filter_word = FilterHandler(handle_option)
-
+    (message_generator, filter_word) = set_handler_behaviour()
     # Creating Agents
-    agent1 = Agent('Agent 1')
-    agent2 = Agent('Agent 2')
+    agent1, agent2 = agent_factory()
 
     # Register handlers and behaviors for both agents
     agent1.register_handle(filter_word)
@@ -54,20 +45,60 @@ async def main():
     except asyncio.CancelledError:
         print("Tasks were cancelled")
 
-def callback():
-    print("Callback ejecutado!")
+def callback(agent1, agent2):
+    # New behaviour and handler
+    new_behaviour_option = [
+        'barsa', 'madrid', 'mun', 'mancity', 'liverpool', 'bayern' , 'milan'
+    ]
+    new_message_generator = SimpleMessageGenerator(alphabet=new_behaviour_option)
+    new_handle_option = 'sun'
+    new_filter_word = FilterHandler(new_handle_option)
 
-def schedule_callback(loop):
-    loop.call_later(5, callback)  # Ejecutar el callback después de 5 segundos
+    # Update agents
+    agent1.register_behaviour(new_message_generator)
+    agent1.register_handle(new_filter_word)
+
+    agent2.register_behaviour(new_message_generator)
+    agent2.register_handle(new_filter_word)
+
+    print("Callback executed! Updated behaviour and handle.")
+
+def schedule_callback(loop, agent_factory: Callable[[], Tuple[Agent, Agent]]):
+    agent1, agent2 = agent_factory()
+    loop.call_later(5, callback, agent1, agent2)  # Ejecutar el callback después de 5 segundos
 
 if __name__ == '__main__':
     # Uncomment to adjust garbage collector thresholds if needed
     # gc.set_threshold(1000, 10, 10)
     # asyncio.run(main(), debug=True)
+
+    def set_handler_behaviour()->Tuple[SimpleMessageGenerator, FilterHandler]:
+        # Setting behaviour
+        behaviour_option = [
+            'hello', 'sun', 'world', 'space', 'moon',
+            'crypto', 'sky', 'ocean', 'universe'
+        ]
+        message_generator = SimpleMessageGenerator(alphabet=behaviour_option)
+
+        # Setting handle
+        handle_option = 'hello'
+        filter_word = FilterHandler(handle_option)
+        return message_generator, filter_word
+
+    agent1 = Agent('Agent 1')
+    agent2 = Agent('Agent 2')
+    
+    def agent_factory() -> Tuple[Agent, Agent]:
+        
+        return agent1, agent2
+ 
     loop = asyncio.get_event_loop()
-    # schedule_callback(loop)
+    schedule_callback(loop, agent_factory)
     try:
-        loop.run_until_complete(main())
+        init = time.time()
+        loop.run_until_complete(main(agent_factory))
+        end = time.time()
+        print(f"Time: {end - init}")
     except KeyboardInterrupt as e:
         print(f"Tasks interrupted {e}")
     finally:

9d93f55e396473fdf8b169acbf9725d0357bb944
ivanjimenezfp@gmail.com <ivanjimenezfp@gmail.com>
2024-06-12 11:22:30 -0400

    integration test to debug

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,44 +1,44 @@
-import unittest
-import logging
-from unittest.mock import MagicMock
-import asyncio
-from agent.simple_agent import SimpleAgent
-from behaviours.simple_message_generator import SimpleMessageGenerator
-from handlers.filter_handler import FilterHandler
+# import unittest
+# import logging
+# from unittest.mock import MagicMock
+# import asyncio
+# from agent.simple_agent import SimpleAgent
+# from behaviours.simple_message_generator import SimpleMessageGenerator
+# from handlers.filter_handler import FilterHandler
 
-class TestAgent(unittest.TestCase):
+# class TestAgent(unittest.TestCase):
 
-    def setUp(self):
-        self.agent = SimpleAgent('TestAgent1')
-        self.other_agent = SimpleAgent('TestAgent2')
-        self.message_generator = MagicMock(spec=SimpleMessageGenerator)
-        self.filter_handler = MagicMock(spec=FilterHandler)
+#     def setUp(self):
+#         self.agent = SimpleAgent('TestAgent1')
+#         self.other_agent = SimpleAgent('TestAgent2')
+#         self.message_generator = MagicMock(spec=SimpleMessageGenerator)
+#         self.filter_handler = MagicMock(spec=FilterHandler)
 
-    def test_connect_both_agents(self):
-        self.agent.set_other_agent(self.other_agent)
-        self.assertEqual(self.agent.other_agent, self.other_agent)
-        logging.info("Configured Agent1 and Agent2.")
+#     def test_connect_both_agents(self):
+#         self.agent.set_other_agent(self.other_agent)
+#         self.assertEqual(self.agent.other_agent, self.other_agent)
+#         logging.info("Configured Agent1 and Agent2.")
 
-    def test_register_handle_agent1(self):
-        self.agent.register_handle(self.filter_handler)
-        self.assertEqual(self.agent.handle, self.filter_handler)
-        logging.info("Agent1 handle configured")
+#     def test_register_handle_agent1(self):
+#         self.agent.register_handle(self.filter_handler)
+#         self.assertEqual(self.agent.handle, self.filter_handler)
+#         logging.info("Agent1 handle configured")
 
-    def test_register_behaviour_agent1(self):
-        self.agent.register_behaviour(self.message_generator)
-        self.assertEqual(self.agent.behaviour, self.message_generator)
-        logging.info("Agent1 behaviour configured")
+#     def test_register_behaviour_agent1(self):
+#         self.agent.register_behaviour(self.message_generator)
+#         self.assertEqual(self.agent.behaviour, self.message_generator)
+#         logging.info("Agent1 behaviour configured")
 
-    def test_register_handle_agent2(self):
-        self.other_agent.register_handle(self.filter_handler)
-        self.assertEqual(self.other_agent.handle, self.filter_handler)
-        logging.info("Agent1 handle configured")
+#     def test_register_handle_agent2(self):
+#         self.other_agent.register_handle(self.filter_handler)
+#         self.assertEqual(self.other_agent.handle, self.filter_handler)
+#         logging.info("Agent1 handle configured")
 
-    def test_register_behaviour_agent2(self):
-        self.other_agent.register_behaviour(self.message_generator)
-        self.assertEqual(self.other_agent.behaviour, self.message_generator)
-        logging.info("Agent2 behaviour configured")
+#     def test_register_behaviour_agent2(self):
+#         self.other_agent.register_behaviour(self.message_generator)
+#         self.assertEqual(self.other_agent.behaviour, self.message_generator)
+#         logging.info("Agent2 behaviour configured")
     
 
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+# if __name__ == '__main__':
+#     unittest.main(verbosity=2)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,52 +1,52 @@
-# import unittest
-# import asyncio
-# from agent.agent import Agent
-# from handlers.filter_handler import FilterHandler
-# from behaviours.simple_message_generator import SimpleMessageGenerator
+import unittest
+import asyncio
+from agent.simple_agent import SimpleAgent
+from handlers.filter_handler import FilterHandler
+from behaviours.simple_message_generator import SimpleMessageGenerator
 
-# class TestIntegration(unittest.TestCase):
-#     def setUp(self):
-#         self.alphabet = [
-#             'hello', 'sun', 'world', 'space', 'moon', 
-#             'crypto', 'sky', 'ocean', 'universe'
-#         ]
-#         self.word = 'hello'
-#         self.agent1 = Agent('Agent 1')
-#         self.agent2 = Agent('Agent 2')
-#         self.filter_word = FilterHandler(self.word, _type='Word')
-#         self.message_generator = SimpleMessageGenerator(self.alphabet)
+class TestIntegration(unittest.TestCase):
+    def setUp(self):
+        self.alphabet = [
+            'hello', 'sun', 'world', 'space', 'moon', 
+            'crypto', 'sky', 'ocean', 'universe'
+        ]
+        self.word = 'hello'
+        self.agent1 = SimpleAgent('Agent 1')
+        self.agent2 = SimpleAgent('Agent 2')
+        self.filter_word = FilterHandler(self.word)
+        self.message_generator = SimpleMessageGenerator(self.alphabet)
 
-#         self.agent1.register_handle(self.filter_word)
-#         self.agent1.register_behaviour(self.message_generator)
-#         self.agent2.register_handle(self.filter_word)
-#         self.agent2.register_behaviour(self.message_generator)
-#         self.agent1.set_other_agent(self.agent2)
-#         self.agent2.set_other_agent(self.agent1)
+        self.agent1.register_handle(self.filter_word)
+        self.agent1.register_behaviour(self.message_generator)
+        self.agent2.register_handle(self.filter_word)
+        self.agent2.register_behaviour(self.message_generator)
+        self.agent1.set_other_agent(self.agent2)
+        self.agent2.set_other_agent(self.agent1)
 
-#     async def test_agent_interaction(self):
-#         # Create and start tasks
-#         task1 = asyncio.create_task(self.agent1.run())
-#         task2 = asyncio.create_task(self.agent2.run())
+    async def test_agent_interaction(self):
+        # Create and start tasks
+        task1 = asyncio.create_task(self.agent1.run())
+        task2 = asyncio.create_task(self.agent2.run())
 
-#         try:
-#             await asyncio.sleep(5)  # Run the agents for 4 seconds
-#         finally:
-#             task1.cancel()
-#             task2.cancel()
+        try:
+            await asyncio.sleep(5)  # Run the agents for 4 seconds
+        finally:
+            task1.cancel()
+            task2.cancel()
 
-#             # Await cancellation of tasks
-#             try:
-#                 await task1
-#             except asyncio.CancelledError:
-#                 print("Agent 1 task was cancelled")
+            # Await cancellation of tasks
+            try:
+                await task1
+            except asyncio.CancelledError:
+                print("Agent 1 task was cancelled")
 
-#             try:
-#                 await task2
-#             except asyncio.CancelledError:
-#                 print("Agent 2 task was cancelled")
+            try:
+                await task2
+            except asyncio.CancelledError:
+                print("Agent 2 task was cancelled")
 
-#     def test_async(self):
-#         asyncio.run(self.test_agent_interaction())
+    def test_async(self):
+        asyncio.run(self.test_agent_interaction())
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
